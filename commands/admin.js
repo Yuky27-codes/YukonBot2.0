a013fc841630cfd100fde75cddacf338bd06e98b
YukyDev <matheus.antonio.ss2016@gmail.com>
2026-05-02 01:04:07 -0300

    Atualização

--- commands/admin.js
+++ commands/admin.js
@@ -3,7 +3,7 @@ module.exports = {
     async execute(client, msg) {
         return msg.reply(`👨‍🚀 *CONTATO COM O COMANDANTE*
 ━━━━━━━━━━━━━━━━━━━━━━
-Precisa de ajuda humana? Clique no link abaixo para falar diretamente com o Matheus:
+Precisa de ajuda humana? Clique no link abaixo para falar diretamente com o Dono da YukonBot:
 
 📲 https://wa.me/5524988268426`);
     }
